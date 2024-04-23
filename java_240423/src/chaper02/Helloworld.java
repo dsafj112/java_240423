7a230b9607cd66fb57a4ca92e18d24a73570e468
dsafj112 <fgghk132@naver.com>
2024-04-23 17:40:10 +0900

    코딩 수업1

---
+++ java_240423/src/chaper02/Helloworld.java
@@ -0,0 +1,23 @@
+package chaper02;
+
+/*
+ * 작성일 : 2024-04023
+ * 작성자 : 나성엽
+ * 내용 : Helloworld 클래스로 문자열 출력 
+ */
+public class Helloworld {
+	public static void main(String[] args) {
+//		method=변수명		
+//		String=문자영 타입
+		String message;	//	변수 선언		
+//		message = "헬로월드"	//변수값 할당(변수 초기화)	
+		message = "헬로월드!";	//변수값 할당(변수 초기화)	
+		System.out.println(message);
+		message="월컴투헬!";
+		System.out.println(message);
+		int a = 2;
+		System.out.println(a);
+		a = 1;
+		System.out.println(a);
+	}
+}
---
+++ java_240423/src/chaper02/VariableInitionalizationExample.java
@@ -0,0 +1,12 @@
+package chaper02;
+
+public class VariableInitionalizationExample {
+
+	public static void main(String[] args) {
+//		변수를 연산해서 변수로 저장
+		int value=20;
+		int result = value+10;
+		System.out.println(result);
+	}
+
+}
